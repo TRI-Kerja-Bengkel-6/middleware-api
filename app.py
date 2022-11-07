41bc2094d321967a81453b8f7348e23e68797828
Hanung Addi Chandra Utomo <hanungutomo45@gmail.com>
2022-11-07 20:58:25 +0700

    update: login auth

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from src.service import service
 import argparse
 import time
 
+import firebase_admin
+import pyrebase
+import json
+from functools import wraps
+from firebase_admin import credentials, auth
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from flask_restx import Resource, Api, reqparse
@@ -14,6 +19,53 @@ api = Api(app=app,
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JSON_SORT_KEYS'] = False
+
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+
+def check_token(f):
+    @ wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            print(request.headers.get('authorization'))
+            user = auth.verify_id_token(request.headers['authorization'].replace('Bearer ',''))
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
+
+#Api route to sign up a new user
+@app.route('/api/signup')
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'},400
+    try:
+        user = auth.create_user(
+               email=email,
+               password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'},200
+    except:
+        return {'message': 'Error creating user'},400
+
+#Api route to get a new token for a valid user
+@app.route('/api/token')
+def token():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        print(user)
+        jwt = user['idToken']
+        return {'token': jwt}, 200
+    except:
+        return {'message': 'There was an error logging in'},400
 
 portainer_namespace = api.namespace('v1', 
                     description='Calling portainer API for multiple purpose')
@@ -28,6 +80,7 @@ class createStack(Resource):
             'subdomain': {'description': 'subdomain ingin digunakan', 'type': 'String', 'required': False}
     })
     @ cross_origin()
+    @ check_token
     def post(self):
 
         # Filter out the request arguments
